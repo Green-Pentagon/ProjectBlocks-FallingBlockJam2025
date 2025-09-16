da66167f1a0c0b56ccdbf6054a783e8a64a269c0
Green-Pentagon <115361212+Green-Pentagon@users.noreply.github.com>
2025-09-16 17:46:03 +0100

    small additions
    dropper now is not visible on camera and has a variable delay + audio queue

--- Assets/Scripts/Actors/Dropper/DropperBehaviour.cs
+++ Assets/Scripts/Actors/Dropper/DropperBehaviour.cs
@@ -4,25 +4,34 @@ using UnityEngine.InputSystem.Controls;
 
 public class DropperBehaviour : MonoBehaviour
 {
-
+    public AudioSource spawnSound;
     public GameObject[] shapePrefabs;
+    public float startDelay = 3.0f;
+    public float endDelay = 0.5f;
+    public float delayDelta = 0.05f;
+    
     Vector3 offset = new Vector3(0.5f, 0.0f, 0.0f);
     int[] spawnerRange = { 0, 10 };
     int[] rotations = {0,90,180,270};
     int cRot;
-    float spawnDelay = 3.0f;
+    float curSpawnDelay;
     bool triggerSpawn = true;
 
     IEnumerator SpawnDelay()
     {
         triggerSpawn = false;
-        yield return new WaitForSeconds(spawnDelay);
+        yield return new WaitForSeconds(curSpawnDelay);
         triggerSpawn = true;
+        if (curSpawnDelay > endDelay)
+        {
+            curSpawnDelay -= delayDelta;
+        }
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        curSpawnDelay = startDelay;
         if (shapePrefabs.Length == 0)
         {
             Debug.LogError("DropperBehaviour.cs: shapePrefabs array is empty!");
@@ -36,6 +45,7 @@ public class DropperBehaviour : MonoBehaviour
         {
             StartCoroutine(SpawnDelay());
             Debug.Log("Object Spawned!");
+            spawnSound.Play();
             transform.position = new Vector3((int)((Random.Range(spawnerRange[0],(spawnerRange[1] / 10.0f) )) * 10),transform.position.y,transform.position.z);
             GameObject rndObj = shapePrefabs[(int)((Random.Range(0.0f, shapePrefabs.Length / 10.0f)) * 10)];
             GameObject obj = Instantiate(rndObj);
